362e6b3e4165457d56d773e8c12f2ef3622e1683
Possible-generation <ajigbotoshojoshua2000@.com>
2023-06-09 03:07:44 +0000

    recursion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,36 +1,23 @@
 #!/usr/bin/python3
-"""
-Query Reddit API recursively for all hot articles of a given subreddit
-"""
+'''
+displays top 10 hot posts
+'''
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="tmp"):
-    """
-        return all hot articles for a given subreddit
-        return None if invalid subreddit given
-    """
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'My User Agent 1.0'})
+def recurse(subreddit, hot_list=[], after=None):
+    user_agent = {'User-agent': 'greg'}
+    sub = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
+                       .format(subreddit, after), headers=user_agent)
 
-    # update url each recursive call with param "after"
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    if after != "tmp":
-        url = url + "?after={}".format(after)
-    r = requests.get(url, headers=headers, allow_redirects=False)
-
-    # append top titles to hot_list
-    results = r.json().get('data', {}).get('children', [])
-    if not results:
-        return hot_list
-    for e in results:
-        hot_list.append(e.get('data').get('title'))
-
-    # get next param "after" else nothing else to recurse
-    after = r.json().get('data').get('after')
-    if not after:
-        return hot_list
-    return (recurse(subreddit, hot_list, after))
+    try:
+        sub = sub.json().get('data')
+        after = sub.get('after')
+        sub = sub.get('children')
+        for obj in sub:
+            hot_list.append(obj['data'].get('title'))
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return(hot_list)
+    except:
+        return(None)
